0775a13b135cbe79c1d179bea3ac0d3a502d1292
ReignBoo <reignboo3281@gmail.com>
2022-09-03 16:32:03 +0800

    Implement guess code method in breaker class

--- lib/breaker.rb
+++ lib/breaker.rb
@@ -8,4 +8,14 @@ class Breaker
     def next_turn
         @turn += 1
     end
+
+    def guess_code()
+        # 0000 to 5555 is used because there are only 6 colors
+        puts "Input your guess to break the code (0000-5555): "
+        guess = gets.chomp until guess.to_i.between?(0, 5555)
+        @guess_result = Array.new()
+        guess
+    end
+
+    
 end
