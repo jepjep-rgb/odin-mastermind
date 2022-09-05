cf18fdb424993cb112cc9c3c816500e775533e1c
ReignBoo <reignboo3281@gmail.com>
2022-09-05 19:02:18 +0800

    Implement basic background colors for string class

---
+++ lib/colorize.rb
@@ -0,0 +1,25 @@
+class String
+  def colorize(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
+  end
+
+  def bg_red
+    colorize(41)
+  end
+
+  def bg_green
+    colorize(42)
+  end
+
+  def bg_blue
+    colorize(44)
+  end
+
+  def bg_magenta
+    colorize(45)
+  end
+
+  def bg_cyan
+    colorize(46)
+  end
+end
