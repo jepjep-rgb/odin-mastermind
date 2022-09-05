e0a2d5a98da42fa7a37437a795dd16aafb6f7bb3
ReignBoo <reignboo3281@gmail.com>
2022-09-05 15:23:08 +0800

    Implement manual create code method in maker class

--- lib/maker.rb
+++ lib/maker.rb
@@ -9,4 +9,11 @@ class Maker
   def randomize_code
     @code_array = Array.new(4) { rand(1...9) }
   end
+
+  def create_code
+    puts 'Please input the code to be decoded (0000-5555): '
+    code = gets.chomp until code.to_i.between?(0,5555)
+    # from '1234' to [1, 2, 3, 4]
+    code.to_i.digits.reverse
+  end
 end
