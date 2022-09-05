1feb34e6bc6ecde655a7daf96d76a693c26dd26c
ReignBoo <reignboo3281@gmail.com>
2022-09-05 18:04:22 +0800

    Implement player choice in code creation in maker class
    The choose type method checks the player type (e.g. Human or Computer),
    and provides the appropriate response per player type. The Computer
    player gets to randomize a 4-digit code, while the Human player gets
    to choose whether to randomize the 4-digit code or to manually input
    the 4-digit code.

--- lib/maker.rb
+++ lib/maker.rb
@@ -6,6 +6,32 @@ class Maker
     @player = player
   end
 
+  def choose_type
+    if @player.type == 'Computer'
+      randomize_code
+    elsif @player.type == 'Human'
+      pick_method
+    end
+  end
+
+  private
+
+  def pick_method
+    puts 'Please choose from the following: '
+    puts '1. Randomize code'
+    puts '2. Manually create code'
+    coding_method = gets.chomp until coding_method.to_i == 1 || coding_method.to_i == 2
+    set_method(coding_method.to_i)
+  end
+
+  def set_method(method)
+    if method == 1
+      randomize_code
+    elsif method == 2
+      create_code
+    end
+  end
+
   def randomize_code
     @code_array = Array.new(4) { rand(1...9) }
   end
